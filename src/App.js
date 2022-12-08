efa831b2352b01472d0bad9f25cdf67504878326
thienthienthienthien <109137446+thienthienthienthien@users.noreply.github.com>
2022-12-08 13:11:49 +0700

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <Navbar />
+        </div>
         <p>
           Quoc Thien
         </p>
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                Quốc Thiện
+            </div>
+            <div>
+                <h1>CHUYÊN: </h1>
+                <div>iPhone cũ mới và các dòng Android như Samsung, Xiaomi, Redmi</div>
+                <div>Phụ kiện Anker: Sạc dự phòng, Củ sạc, Cáp sạc...</div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
