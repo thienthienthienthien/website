9c9be023d90ba4548b31f7ec9fe5908d189ae207
thienthienthienthien <109137446+thienthienthienthien@users.noreply.github.com>
2022-12-08 18:55:38 +0700

    Update

--- src/Components/Mobile/Products/Product1.js
+++ src/Components/Mobile/Products/Product1.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import logo1 from '../../../assets/img/qq.jpg';
+import './Product.css';
 
 const Product1 = () => {
     return (
         <div>
-            iPhone 14 Pro Max
+            <div>
+
+            </div>
+
+            <div>
+                <img src={logo1} />
+            </div>
+
         </div>
     )
 }
