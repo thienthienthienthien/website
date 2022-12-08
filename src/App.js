6b86094b90081416df7db3b83c2128bd5cc86d8a
thienthienthienthien <109137446+thienthienthienthien@users.noreply.github.com>
2022-12-08 17:31:03 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
 
       <div>
         <Navbar />
       </div>
 
-      <div>
-        Quoc Thien
-      </div>
+
     </>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import "./Navbar.css";
 
 const Navbar = () => {
     return (
         <>
-            <h1>
-                Quốc Thiện
-            </h1>
-            <div>
-                <h2>CHUYÊN: </h2>
-                <div>iPhone cũ mới và các dòng Android như Samsung, Xiaomi, Redmi</div>
-                <div>Phụ kiện Anker: Sạc dự phòng, Củ sạc, Cáp sạc...</div>
+            <div className="nav-container">
+                <h1 className="nav-1" >
+                    Quốc Thiện
+                </h1>
+                <div className="nav-2">
+                    <h3>CHUYÊN: </h3>
+                    <div>- iPhone cũ mới và các dòng Android như Samsung, Xiaomi, Redmi</div>
+                    <div>- Phụ kiện Anker: Sạc dự phòng, Củ sạc, Cáp sạc...</div>
+                </div>
             </div>
         </>
     )
